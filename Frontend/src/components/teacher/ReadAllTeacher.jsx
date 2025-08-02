68b99e407a58ccd1049fba69fd02a5ccd484883a
Alish-Shakya <alishshakya44@gmail.com>
2025-08-02 20:43:20 +0545

    teacher deleted successfully

--- Frontend/src/components/teacher/ReadAllTeacher.jsx
+++ Frontend/src/components/teacher/ReadAllTeacher.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import Swal from "sweetalert2";
 
 const ReadAllTeacher = () => {
   let [teachers, setTeachers] = useState([]);
@@ -22,9 +23,40 @@ const ReadAllTeacher = () => {
     getTeachers();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      let data = await axios.delete(
+        `http://localhost:3000/teacher/deleteTeacher/${id}`
+      );
+      console.log(data);
+      getTeachers();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const alertDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        handleDelete(id);
+      }
+    });
+  };
+
   return (
     <div>
-      <h1>All Teachers</h1>
+      <h1 className="text-4xl font-extralight justify-center m-10">
+        All Teachers
+      </h1>
       {teachers.map((value) => (
         <div
           key={value._id}
@@ -52,7 +84,10 @@ const ReadAllTeacher = () => {
               Edit
             </button>
 
-            <button className="border rounded-2xl px-3 mr-5 mt-3 hover:bg-amber-50 cursor-pointer">
+            <button
+              className="border rounded-2xl px-3 mr-5 mt-3 hover:bg-amber-50 cursor-pointer"
+              onClick={() => alertDelete(value._id)}
+            >
               Delete
             </button>
           </div>
